6e844b107877a668f5121c7b6b2955affae700fd
saif ahmed <saifa5684@gmail.com>
2023-12-14 16:29:18 +0530

    ajax pagination

---
+++ ajax-pagination.php
@@ -0,0 +1,58 @@
+<?php
+($conn = mysqli_connect("localhost", "root", "password", "demo")) or die("connection failed");
+
+$limitPerPage = 3;
+$page = isset($_POST['page_no']) ? $_POST['page_no'] : 1;
+
+$offset = ($page - 1) * $limitPerPage;
+
+$sql = "SELECT * FROM students LIMIT $offset, $limitPerPage";
+($result = mysqli_query($conn, $sql)) or die("query unsuccessful");
+
+$output = '';
+
+if (mysqli_num_rows($result) > 0) {
+    $output .= '
+        <table class="table table-striped" style="border:1px solid black">
+        <h1 class="text-center">PAGINATION WITH AJAX</h1>
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">NAME</th>
+            </tr>
+        </thead>
+        <tbody>';
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $output .=
+            '<tr>
+                        <td>' .
+            $row['s_id'] .
+            '</td>
+                        <td>' .
+            $row['s_name'] .
+            '</td>
+                    </tr>';
+    }
+
+    $output .= '</tbody></table>';
+
+    $studentTotal = "SELECT COUNT(*) AS total FROM students";
+    ($records = mysqli_query($conn, $studentTotal)) or die("query unsuccessful");
+    $totalRecords = mysqli_fetch_assoc($records)['total'];
+    $totalPages = ceil($totalRecords / $limitPerPage);
+
+    $output .= '<ul class="pagination pagination-lg" id="pagination">';
+    for ($i = 1; $i <= $totalPages; $i++) {
+        if ($i == $page) {
+            $activeClass = "active";
+        } else {
+            $activeClass = "";
+        }
+        $output .= '<li id="' . $i . '" class="' . $activeClass . '"><a class="page-link" href="#">' . $i . '</a></li>';
+    }
+    $output .= '</ul>';
+
+    echo $output;
+}
+?>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-5">
+                    <div id="table-data"></div>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
+
+        <script>
+            $(document).ready(function () {
+                function loadTable(page) {
+                    $.ajax({
+                        url: "ajax-pagination.php",
+                        type: "POST",
+                        data: { page_no: page },
+                        success: function (data) {
+                            $("#table-data").html(data);
+                        },
+                    });
+                }
+
+                loadTable();
+
+                $(document).on("click", "#pagination li", function (e) {
+                    e.preventDefault();
+                    var pageId = $(this).attr("id");
+
+                    loadTable(pageId);
+                });
+            });
+        </script>
+    </body>
+</html>
